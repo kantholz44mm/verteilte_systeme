730f09362d5ca06139663c09a7539eaa6290ad5a
Matthias Raba <matthias@raba.live>
2026-03-23 11:35:49 +0100

    added target send address

--- aufgabe1/src/main.rs
+++ aufgabe1/src/main.rs
@@ -1,6 +1,7 @@
-use std::{env, io::{Read, Write}, net::{TcpListener, UdpSocket}};
+use std::{env, io::{Read, Write}, net::{IpAddr, Ipv4Addr, TcpListener, TcpStream, UdpSocket}};
 
 const DEFAULT_LISTEN_PORT: u16 = 8080;
+const DEFAULT_SEND_ADDRESS: (IpAddr, u16) = (IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8090);
 const DEFAULT_OPERATION: Operation = Operation::Increment;
 const DEFAULT_SOCKETTYPE: SocketType = SocketType::UDP;
 
@@ -79,24 +80,25 @@ pub fn lex_number(mut input: &str) -> Option<i64> {
 }
 
 
-fn run_udp(port: u16, operation: Operation) -> std::io::Result<()> {
+fn run_udp(port: u16, operation: Operation, send_address: (IpAddr, u16)) -> std::io::Result<()> {
 
     let socket = UdpSocket::bind(format!("127.0.0.1:{}", port))?;
 
     loop {
         let mut buf = [0; 256];
-        let (amt, src) = socket.recv_from(&mut buf)?;
+        let (amt, _src) = socket.recv_from(&mut buf)?;
         let buf = &mut buf[..amt];
         let operand = lex_number(str::from_utf8(buf).unwrap_or("")).unwrap_or(0);
         let result = operation.execute_on_operand(operand);
         let response = result.to_string();
         let response_buffer = response.as_bytes();
 
-        socket.send_to(response_buffer, src)?;
+        let send_socket = UdpSocket::bind("127.0.0.1")?;
+        send_socket.send_to(response_buffer, send_address)?;
     }
 }
 
-fn run_tcp(port: u16, operation: Operation) -> std::io::Result<()> {
+fn run_tcp(port: u16, operation: Operation, send_address: (IpAddr, u16)) -> std::io::Result<()> {
 
     let socket = TcpListener::bind(format!("127.0.0.1:{}", port))?;
 
@@ -112,22 +114,37 @@ fn run_tcp(port: u16, operation: Operation) -> std::io::Result<()> {
         let response = result.to_string();
         let response_buffer = response.as_bytes();
 
-        src_stream.write(response_buffer)?;
-        src_stream.flush();
+        let mut send_stream = TcpStream::connect(send_address)?;
+        send_stream.write(response_buffer)?;
+        send_stream.flush();
     }
+}
+
+fn parse_address_port_pair(input: &str) -> (IpAddr, u16) {
+    let tokens = input.split(":").collect::<Vec<&str>>();
+
+    if tokens.len() != 2 {
+        return DEFAULT_SEND_ADDRESS;
+    }
+
+    let address = tokens[0].parse::<IpAddr>().unwrap_or(DEFAULT_SEND_ADDRESS.0);
+    let port = tokens[1].parse::<u16>().unwrap_or(DEFAULT_SEND_ADDRESS.1);
+
+    (address, port)
 }
 
 
 fn main() -> std::io::Result<()> {
     {
         let listen_port = env::var("LISTEN_PORT").map_or(DEFAULT_LISTEN_PORT, |p| u16::from_str_radix(&p, 10).unwrap_or(DEFAULT_LISTEN_PORT));
+        let send_address = env::var("SEND_ADDRESS").map_or(DEFAULT_SEND_ADDRESS, |s| parse_address_port_pair(&s));
         let operation = env::var("OPERATION").map_or(DEFAULT_OPERATION, |p| Operation::from(p.as_str()));
         let socket_type = env::var("SOCKETTYPE").map_or(DEFAULT_SOCKETTYPE, |p| SocketType::from(p.as_str()));
 
-        if socket_type == SocketType::TCP {
-            run_tcp(listen_port, operation)
-        } else {
-            run_udp(listen_port, operation)
+        let runner = if socket_type == SocketType::TCP { run_tcp } else { run_udp };
+        loop {
+            let result = runner(listen_port, operation, send_address);
+            println!("Runner exited with state: {:?}", result);
         }
     }
 }
